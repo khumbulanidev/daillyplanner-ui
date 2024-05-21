d25bc0483b834882dfff390c76cdfb6f0eac1655
khumbulani ndlovu <kndlovusr@gmail.com>
2024-05-20 23:29:33 -0500

    add day service

---
+++ src/app/services/daylist-service/daylist.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DaylistService {
+ BASE_URL="";
+  constructor() { }
+
+  getDayList(){
+
+  }
+}
