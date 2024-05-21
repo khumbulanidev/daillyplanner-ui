f474dd22e0bd84d9d2d3307e22259664312c4e33
khumbulani ndlovu <kndlovusr@gmail.com>
2024-05-20 23:31:00 -0500

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DaysComponent } from './days/days.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { DaylistComponent } from './daylist/daylist.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path : "home" , component: HomeComponent},
+{path : "day" , component : DaysComponent},
+{path : "task" , component : TasksComponent},
+{path : "days",component : DaylistComponent }
+
+];
