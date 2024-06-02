3c0c4c980a818a6c5314d710205725719ceb899c
khumbulani ndlovu <kndlovusr@gmail.com>
2024-06-02 18:36:41 -0500

    add constants file

---
+++ src/app/constants/DailyPlannerConstants.ts
@@ -0,0 +1,8 @@
+export const DAY_SAVED_SUCCESSULLY = 'Day saved successfully';
+export const COLON = ' : ';
+export const SUCCESS = 'success';
+export const MESSAGE_CONTENT = 'Message Content';
+export const DANGER= 'danger';
+export const ERROR_MESSAGE ='Error occured :';
+
+ 
