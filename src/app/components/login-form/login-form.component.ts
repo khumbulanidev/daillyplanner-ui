21c3048f1a8452fa6f0019675702f88d109412ea
khumbulani.ndlovu <khumbulani.ndlovu@fdmgrouop.com>
2025-06-04 18:00:07 -0500

    Add login form component

---
+++ src/app/components/login-form/login-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-login-form',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './login-form.component.html',
+  styleUrl: './login-form.component.css'
+})
+export class LoginFormComponent {
+
+}
