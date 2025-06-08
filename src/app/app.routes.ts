68aef2aa9f67c6970f061d9b794cd76ecde93c04
khumbulani.ndlovu <khumbulani.ndlovu@fdmgrouop.com>
2025-06-07 22:23:50 -0500

    Add more routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,29 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DaysComponent } from './days/days.component';
 import { TasksComponent } from './tasks/tasks.component';
-import { DaylistComponent } from './daylist/daylist.component';
+import { ViewComponent } from './days/view/view.component';
+import { ImageExampleComponent } from './image-example/image-example.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { RevisionComponent } from './revision/revision.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { BouncingTextComponent } from './components/bouncing-text/bouncing-text.component';
+import { DaylistComponent } from './components/daylist/daylist.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
 {path : "home" , component: HomeComponent},
 {path : "day" , component : DaysComponent},
-{path : "task" , component : TasksComponent},
-{path : "days",component : DaylistComponent }
+{path : "tasks/:id" , component : TasksComponent},
+{path : "days",component : DaylistComponent },
+{path : "view/:id/:date" , component : ViewComponent},
+{path : "add-task/:dateId" , component : AddTaskComponent},
+{path: "img" , component : ImageExampleComponent},
+{path:"side-menu", component : SideMenuComponent},
+{path : "bounce" , component : BouncingTextComponent},
+{path : "revision" , component : RevisionComponent},
+{path : "sign-up" , component : SignUpComponent},
+{path : "dashboard" , component : DashboardComponent},
+{path : "" , component: HomeComponent}
 
 ];
