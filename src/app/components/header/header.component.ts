cfb32b2765f7f06ddaf77d861138a3330c937b5d
khumbulani.ndlovu <khumbulani.ndlovu@fdmgrouop.com>
2025-07-10 10:32:43 -0500

    add header component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,44 @@
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication/authentication.service';
+import { Subscription } from 'rxjs';
+import { User } from '../../models/user';
+import { Router, RouterModule } from '@angular/router';
+
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent implements OnInit, OnDestroy{
+
+
+authService = inject(AuthenticationService);
+router = inject(Router)
+sub = new Subscription();
+isAuthenticated = false;
+  user!: User | null;
+
+ngOnInit(): void {
+  this.sub = this.authService.userSubject.subscribe(user =>{
+   this.isAuthenticated = !!user; 
+   
+   this.user = user;
+   console.log('User in header ', user);
+   this.isAuthenticated ? this.router.navigateByUrl('/dashboard') : this.router.navigateByUrl('/login')
+  })
+}
+
+
+logout() {
+this.isAuthenticated = false;
+this.router.navigateByUrl('/home')
+}
+
+ngOnDestroy(): void {
+  this.sub.unsubscribe();
+}
+
+}
