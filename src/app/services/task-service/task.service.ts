7811e3224486ca24357430c5347baf74e87207db
khumbulani.ndlovu <khumbulani.ndlovu@fdmgrouop.com>
2025-08-12 13:47:37 -0500

    add task service

--- src/app/services/task-service/task.service.ts
+++ src/app/services/task-service/task.service.ts
@@ -1,25 +1,45 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { TaskDto } from '../../models/TaskDto';
 import { LoggerService } from '../logger/logger.service';
 import { NonNullableFormBuilder } from '@angular/forms';
+import { Task } from 'zone.js/lib/zone-impl';
+import { BASE_URL, TASK_URL } from '../../constants/DailyPlannerConstants';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
 
-  private tasksUrl: string= 'http://localhost:8080/api/v1/tasks'
-  constructor(private http : HttpClient, private logger : LoggerService) { }
+  
+ 
+  http = inject(HttpClient);
+  logger = inject(LoggerService);
+  constructor() {}
 
-  getTasksByDayId(dayId : number): Observable<TaskDto[]>{
-    return this.http.get<TaskDto[]>(this.tasksUrl+'/'+dayId);
-     
-      }
-    
-      saveTask(taskDto : TaskDto){
-          this.http.post(this.tasksUrl+'/save',taskDto);
-        
-      }
+  getTasksByDayId(dayId: number): Observable<TaskDto[]> {
+    return this.http.get<TaskDto[]>(BASE_URL + TASK_URL + '/' + dayId);
+  }
+
+   getTaskById(taskId: number): Observable<TaskDto> {
+    return this.http.get<TaskDto>(BASE_URL + TASK_URL + '/task/' + taskId);
+  }
+
+  getTasksForToday(date : string): Observable<TaskDto[]> {
+    let day = new Date(date);
+    return this.http.get<TaskDto[]>(BASE_URL + TASK_URL + `/today/${date}`);
+  }
+
+  saveTask(taskDto: TaskDto):Observable<TaskDto> {
+    return this.http.post<TaskDto>(BASE_URL + TASK_URL + '/save', taskDto);
+  }
+
+  updateTask(taskDto: TaskDto): Observable<TaskDto> {
+     return this.http.put<TaskDto>(BASE_URL + TASK_URL +'/update', taskDto);
+  }
+
+   deleteById(taskId: number):Observable<TaskDto> {
+    return this.http.delete<TaskDto>(BASE_URL + TASK_URL + '/delete/'+taskId);
+  }
 }
