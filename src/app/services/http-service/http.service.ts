3dc615e88d51252ba270b54c285e76243f466412
khumbulani.ndlovu <khumbulani.ndlovu@fdmgrouop.com>
2025-08-12 13:48:09 -0500

    add services

--- src/app/services/http-service/http.service.ts
+++ src/app/services/http-service/http.service.ts
@@ -1,17 +1,27 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { DayDto } from '../../models/DayDto';
 import { Observable } from 'rxjs';
 import { LoggerService } from '../logger/logger.service';
+import { TaskDto } from '../../models/TaskDto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  private url: string ='http://localhost:8080/api/v1/days';
+   url : string ='http://localhost:9200/api/v1/days';
+   tasksUrl : string= 'http://localhost:9200/api/v1/tasks';
 
-  constructor(private http : HttpClient, private logger : LoggerService) { }
+  //services
+  http = inject(HttpClient);
+  logger = inject(LoggerService);
+
+  constructor() { }
+  //pass in date and 
+  getTasksForToday(): Observable<DayDto[]>{
+    return this.http.get<DayDto[]>(this.tasksUrl);
+  }
   getDays(): Observable<DayDto[]>{
     return this.http.get<DayDto[]>(this.url);
   }
@@ -22,4 +32,6 @@ export class HttpService {
   deleteDay(id:number) {
     return this.http.delete(this.url+"/delete/"+id);
   }
+
+
 }
--- src/app/services/logger/logger.service.ts
+++ src/app/services/logger/logger.service.ts
@@ -8,14 +8,20 @@ export class LoggerService {
   constructor() { }
 log(msg : any){
   console.log(msg);
-
 }
+
+detailedLog(msg : string, value : any){
+  console.log(msg, value);
+}
+
 error(msg :any){
   console.error(msg);
 }
 warn(msg : any){
  console.warn(msg);
 };
+
+
 
  
   
