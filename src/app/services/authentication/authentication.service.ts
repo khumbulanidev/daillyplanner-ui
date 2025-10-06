be6a123e3c837cf7085f8bc56b575ec0a01513c2
khumbulani.ndlovu <khumbulani.ndlovu@fdmgrouop.com>
2025-10-06 16:43:06 -0500

    refactor authentication service add refreshtoken method

--- src/app/services/authentication/authentication.service.ts
+++ src/app/services/authentication/authentication.service.ts
@@ -1,22 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
-import { LOGIN_API, BASE_URL } from '../../constants/DailyPlannerConstants';
+import { LOGIN_API, BASE_URL, REFRESH_TOKEN } from '../../constants/DailyPlannerConstants';
 import { LoginDto } from '../../dto/LoginDto';
 import { ApiResponseDto } from '../../dto/ApiResponseDto';
 import { User } from '../../models/user';
 import { RefreshToken } from '../../models/RefreshToken';
+import { RefreshTokenDto } from '../../dto/RefreshTokenDto';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationService {
+
   userSubject = new BehaviorSubject<User | null>(null);
   user = this.userSubject.asObservable();
+  tokenExpired$ : BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
 
   httpClient = inject(HttpClient);
 
-  changeUser(user: User) {
+  changeUser(user: User | null) {
     this.userSubject.next(user);
   }
 
@@ -53,4 +56,18 @@ export class AuthenticationService {
     );
     this.userSubject.next(user);
   }
+
+
+    getAccessToken() {
+    return this.userSubject.value?.token;
+  }
+
+    refreshToken(refreshToken: RefreshTokenDto): Observable<ApiResponseDto> {
+    return this.httpClient.post<ApiResponseDto>(
+      BASE_URL + REFRESH_TOKEN,
+      refreshToken
+    );
+  }
+
+ 
 }
