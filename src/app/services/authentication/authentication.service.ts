b50dcab6f98eae990fa07c8a3786a90a4ee83aa7
khumbulani.ndlovu <khumbulani.ndlovu@fdmgrouop.com>
2025-11-25 16:42:07 -0600

    merge dev into main

--- src/app/services/authentication/authentication.service.ts
+++ src/app/services/authentication/authentication.service.ts
@@ -1,7 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
-import { LOGIN_API, BASE_URL, REFRESH_TOKEN, USER_API, LOGOUT_API } from '../../constants/DailyPlannerConstants';
+import {
+  LOGIN_API,
+  BASE_URL,
+  REFRESH_TOKEN,
+  USER_API,
+  LOGOUT_API,
+} from '../../constants/DailyPlannerConstants';
 import { LoginDto } from '../../dto/LoginDto';
 import { ApiResponseDto } from '../../dto/ApiResponseDto';
 import { User } from '../../models/user';
@@ -16,16 +22,12 @@ import { Role } from '../../models/role';
   providedIn: 'root',
 })
 export class AuthenticationService {
- 
-
   userSubject = new BehaviorSubject<User | null>(null);
   user = this.userSubject.asObservable();
-  tokenExpired$ : BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
-  router = inject(Router)
+  tokenExpired$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+  router = inject(Router);
   httpClient = inject(HttpClient);
-  toastService = inject(ToastrService)
-
- 
+  toastService = inject(ToastrService);
 
   changeUser(user: User | null) {
     this.userSubject.next(user);
@@ -71,43 +73,43 @@ export class AuthenticationService {
     this.userSubject.next(user);
   }
 
-
-    getAccessToken() {
+  getAccessToken() {
     return this.userSubject.value?.token;
   }
 
-    refreshToken(refreshToken: RefreshTokenDto): Observable<ApiResponseDto> {
+  refreshToken(refreshToken: RefreshTokenDto): Observable<ApiResponseDto> {
     return this.httpClient.post<ApiResponseDto>(
       BASE_URL + REFRESH_TOKEN,
       refreshToken
     );
   }
 
-   logout() {
+  logout() {
     this.clearLocalStorage();
-   
-   //send request to backend to clear token and refresh token
-   this.logoutFromServer().subscribe({
-    next : response => {
-      this.toastService.success("Logout successful","Message");
-      console.log("Response for logout ", response)
-    },
-    error : err =>{
-      console.log("Error occurred whilst logging out ", err)
-    }
-   });
-   this.userSubject.next(null);
+
+    //send request to backend to clear token and refresh token
+    this.logoutFromServer().subscribe({
+      next: (response) => {
+        this.toastService.success('Logout successful', 'Message');
+        console.log('Response for logout ', response);
+      },
+      error: (err) => {
+        console.log('Error occurred whilst logging out ', err);
+      },
+    });
+    this.userSubject.next(null);
     this.router.navigateByUrl('/home');
   }
 
-  clearLocalStorage()
-  {
-     localStorage.removeItem('email');
-     localStorage.removeItem('refTok');
+  clearLocalStorage() {
+    localStorage.removeItem('email');
+    localStorage.removeItem('refTok');
   }
 
-  logoutFromServer(): Observable<LogoutDto>{
-    return this.httpClient.post<LogoutDto>(BASE_URL + LOGOUT_API, this.userSubject.value?.email ?? '');
+  logoutFromServer(): Observable<LogoutDto> {
+    return this.httpClient.post<LogoutDto>(
+      BASE_URL + LOGOUT_API,
+      this.userSubject.value?.email ?? ''
+    );
   }
-
 }
