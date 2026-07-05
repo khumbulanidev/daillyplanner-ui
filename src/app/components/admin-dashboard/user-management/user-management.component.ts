5c4789c81bdbcc6412c72dcf4a2d5be092c151b5
khumbulani.ndlovu <kndlovudev@gmail.com>
2026-07-05 04:02:52 -0500

    Refactor user management

--- src/app/components/admin-dashboard/user-management/user-management.component.ts
+++ src/app/components/admin-dashboard/user-management/user-management.component.ts
@@ -5,6 +5,8 @@ import { UserDto } from '../../../dto/user-dto';
 import { UserService } from '../../../services/user/user.service';
 import { ERROR_RETRIEVING_USERS } from '../../../constants/DailyPlannerConstants';
 import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { state } from '@angular/animations';
 
 @Component({
   selector: 'app-user-management',
@@ -29,7 +31,8 @@ allCheckboxesChecked: any;
 isACheckboxChecked: any;
 tasksToSave: any;
 userService = inject(UserService);
-toastService = inject(ToastrService)
+toastService = inject(ToastrService);
+router = inject(Router)
 
 ngOnInit(): void {
  this.userService.getUsers().subscribe({
@@ -65,7 +68,27 @@ saveTasks() {
 deleteAll() {
 
 }
-//get all users from the db
+
+
+/**
+ * 
+ * @param email 
+ */
+viewUser(user: UserDto) {
+this.router.navigate(['/user'], {state : user});
+
+}
+
+//get all users from the db --DONE
+
 //admin making changes cannot delete his account but can make other changes
+
 //add a table to the ui to display the list of users
+
+//add eye icon for view 
+
+//create a page to edit user opened from view icon
+
+//delete icon opens delete popup and prompts user to enter a pin to delete
+
 }
