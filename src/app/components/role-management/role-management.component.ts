f987acffddc5c5421184b01dbd4a44e56fd0bfc8
khumbulani.ndlovu <kndlovudev@gmail.com>
2026-07-21 13:03:54 -0500

    Add role table

--- src/app/components/role-management/role-management.component.ts
+++ src/app/components/role-management/role-management.component.ts
@@ -2,34 +2,69 @@ import { Component, inject, OnInit } from '@angular/core';
 import { TableModule } from "primeng/table";
 import { Role } from '../../models/role';
 import { RoleService } from '../../services/role.service';
+import { PopupModalComponent } from "../popup-modal/popup-modal.component";
+import { NgClass } from '@angular/common';
+import { Router } from '@angular/router';
+import { Toast } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-role-management',
   standalone: true,
-  imports: [ TableModule],
+  imports: [TableModule, PopupModalComponent, NgClass],
   templateUrl: './role-management.component.html',
   styleUrl: './role-management.component.css'
 })
 export class RoleManagementComponent implements OnInit {
 
+
+
 //services
-roleService = inject(RoleService)
+roleService = inject(RoleService);
+toastService = inject(Toast);
+router = inject(Router);
 roles: any;
+deleteMsg: string = '';
+data: any;
+showModal: boolean =false;
 
 removeRow(id: number) {
+  this.data = id;
+  this.showModal = true;
 
 }
-viewUser(role: Role) {
 
+viewUser(role: Role) {
+//this.router.navigateByUrl('');
 }
   
 
+closeModal($event: string) {
 
+}
+
+deleteItem(id: number) {
+  this.roleService.delete(id).subscribe({
+    next : response =>{
+
+    }, 
+    error : error=>{
+      console.log(error)
+    }
+  })
+
+}
 
 constructor(){
  
 }
   ngOnInit(): void {
+
+    this.roleService.getAll().subscribe({
+      next: response => {
+        this.roles = response;
+      }
+    })
   }
 
 }
